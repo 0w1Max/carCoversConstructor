1a2fac816444c94bbf659dcaf62f7c9c974509f7
Макс <0w1.go.0w1@gmail.com>
2022-04-21 02:04:39 +0300

    Написал функции для получения массивов с параметрами авточехлов

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //  План приложения "Конструктор автомобильных чехлов"
 //  +1. Сделать черновую разметку
 //  2. Написать JS логику для выбора значений конструтора и отрисовать их в разметке
-////  2.1. Найти все DOM-элементы разметки
+////  +2.1. Найти все DOM-элементы разметки
 ////  2.2. Добавить все значения для конструктора (материал, цвет чехла и т.д.)
 ////  2.3. Написать функцию-конструктор для создания объекта со всеми значениями
 ////  2.4. Добавить события для вывода значений в разметке
@@ -14,3 +14,28 @@ const selectColorOptions = selectColor.querySelector('option');
 
 const materialText = document.querySelector('.add-cover-text__material');
 const colorText = document.querySelector('.add-cover-text__color');
+
+function getMaterials () {
+  const materials = [];
+  
+  const materialOptions = document.querySelectorAll('.select-item__material option');
+ 
+  materialOptions.forEach((material) => materials.push(material.value));
+  
+  return materials;
+}
+
+function getColors () {
+  const colors = [];
+  
+  const colorOptions = document.querySelectorAll('.select-item__color option');
+ 
+  colorOptions.forEach((color) => colors.push(color.value));
+  
+  return colors;
+}
+
+const materials = getMaterials();
+console.log(materials);
+const colors = getColors();
+console.log(colors);
