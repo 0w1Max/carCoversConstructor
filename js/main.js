b5f030a320d05ae48143434a499528cdeaef7201
Макс <0w1.go.0w1@gmail.com>
2022-04-22 14:43:30 +0300

    Раскомментировал строки кода

--- js/main.js
+++ js/main.js
@@ -1,56 +1,56 @@
 import {getValuesArray, getTextContentArray, getObj} from './util.js';
 import './rendering.js';
 
-// const selectMaterial = document.querySelector('.select-item__material');
-// const selectMaterialOptions = selectMaterial.querySelector('option');
-// const selectColor = document.querySelector('.select-item__color');
-// const selectColorOptions = selectColor.querySelector('option');
+const selectMaterial = document.querySelector('.select-item__material');
+const selectMaterialOptions = selectMaterial.querySelector('option');
+const selectColor = document.querySelector('.select-item__color');
+const selectColorOptions = selectColor.querySelector('option');
 
-// const materialText = document.querySelector('.add-cover-text__material');
-// const colorText = document.querySelector('.add-cover-text__color');
+const materialText = document.querySelector('.add-cover-text__material');
+const colorText = document.querySelector('.add-cover-text__color');
 
-// const showColor = document.querySelector('.show-color');
+const showColor = document.querySelector('.show-color');
 
-// const materials = getValuesArray('.select-item__material option');
-// const colors = getValuesArray('.select-item__color option');
-// console.log(materials);
-// console.log(colors);
+const materials = getValuesArray('.select-item__material option');
+const colors = getValuesArray('.select-item__color option');
+console.log(materials);
+console.log(colors);
 
-// const materialsTextContent = getTextContentArray('.select-item__material option');
-// const colorsTextContent = getTextContentArray('.select-item__color option');
-// console.log(materialsTextContent);
-// console.log(colorsTextContent);
+const materialsTextContent = getTextContentArray('.select-item__material option');
+const colorsTextContent = getTextContentArray('.select-item__color option');
+console.log(materialsTextContent);
+console.log(colorsTextContent);
 
-// const materialsObj = getObj(materials, materialsTextContent);
-// const colorsObj = getObj(colors, colorsTextContent);
-// console.log(materialsObj);
-// console.log(colorsObj);
+const materialsObj = getObj(materials, materialsTextContent);
+const colorsObj = getObj(colors, colorsTextContent);
+console.log(materialsObj);
+console.log(colorsObj);
 
-// function carCover (materials, colors) {
-//   this.materials = materials;
-//   this.colors = colors;
-// }
+function carCover (materials, colors) {
+  this.materials = materials;
+  this.colors = colors;
+}
 
-// const newCarCover = new carCover(materialsObj, colorsObj);
-// console.log(newCarCover);
+const newCarCover = new carCover(materialsObj, colorsObj);
+console.log(newCarCover);
 
-// if (selectMaterialOptions) {
-//   materialText.textContent = newCarCover.materials[selectMaterialOptions.value];
-// }
+if (selectMaterialOptions) {
+  materialText.textContent = newCarCover.materials[selectMaterialOptions.value];
+}
 
-// if (selectColorOptions) {
-//   colorText.textContent = newCarCover.colors[selectColorOptions.value];
-//   showColor.style.background = 'orange';
-// }
+if (selectColorOptions) {
+  colorText.textContent = newCarCover.colors[selectColorOptions.value];
+  showColor.style.background = 'orange';
+}
 
-// function materialChangeHandler (evt) {
-//   materialText.textContent = newCarCover.materials[evt.target.value];
-// }
+function materialChangeHandler (evt) {
+  materialText.textContent = newCarCover.materials[evt.target.value];
+}
 
-// function colorChangeHandler (evt) {
-//   colorText.textContent = newCarCover.colors[evt.target.value];
-// }
+function colorChangeHandler (evt) {
+  colorText.textContent = newCarCover.colors[evt.target.value];
+}
 
-// selectMaterial.addEventListener('change', materialChangeHandler);
+selectMaterial.addEventListener('change', materialChangeHandler);
 
-// selectColor.addEventListener('change', colorChangeHandler);
+selectColor.addEventListener('change', colorChangeHandler);
