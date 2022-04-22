2ed4653f45d92438a4e68d57a77543e63c0ca4cc
Макс <0w1.go.0w1@gmail.com>
2022-04-22 14:05:57 +0300

    Переписал код. Теперь параметры авточехлов в объектах моковых данных. На основе этих данных рендерится DOM-дерево

---
+++ js/mock.js
@@ -0,0 +1,28 @@
+const COLORS = {
+    'black': 'Чёрный',
+    'blue': 'Синий',
+    'gray': 'Серый',
+    'green': 'Зелёный',
+    'orange': 'Оранжевый',
+    'red': 'Красный',
+    'white': 'Белый'
+};
+
+const HEX_COLORS = {
+    'black': '#000',
+    'blue': 'Синий',
+    'gray': 'Серый',
+    'green': 'Зелёный',
+    'orange': 'Оранжевый',
+    'red': 'Красный',
+    'white': '#fff'
+};
+
+const MATERIALS = {
+    'material-1': "material 1",
+    'material-2': "material 2",
+    'material-3': "material 3",
+    'material-4': "material 4"
+};
+
+export {COLORS, HEX_COLORS, MATERIALS};
