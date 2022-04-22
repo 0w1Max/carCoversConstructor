6f4095693130c5a0693d46467ce6c2c310ecccd2
Макс <0w1.go.0w1@gmail.com>
2022-04-22 14:34:44 +0300

    Переместил функцию для рендеринга в отдельный модуль

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import {COLORS, HEX_COLORS, MATERIALS} from './mock.js';
-import {getValuesArray, getTextContentArray, getObj} from "./util.js";
+import {getValuesArray, getTextContentArray, getObj} from './util.js';
+import {renderTemplate} from './rendering.js';
 
 // const selectMaterial = document.querySelector('.select-item__material');
 // const selectMaterialOptions = selectMaterial.querySelector('option');
@@ -10,38 +10,6 @@ import {getValuesArray, getTextContentArray, getObj} from "./util.js";
 // const colorText = document.querySelector('.add-cover-text__color');
 
 // const showColor = document.querySelector('.show-color');
-
-const body = document.querySelector('body');
-const coverTemplate = document.querySelector('#cover-template').content;
-const fragment = document.createDocumentFragment();
-const cloneTemplate = coverTemplate.cloneNode(true);
-
-function getMock (select, option, obj) {
-  for (let key in obj) {
-    const newOption = option.cloneNode(true);
-
-    newOption.value = key;
-    newOption.textContent = obj[key];
-    
-    select.appendChild(newOption);
-  }
-}
-
-function renderTemplate () {
-  const selectColor = cloneTemplate.querySelector('.select-item__color');
-  const optionColor = selectColor.querySelector('option');
-  const selectMaterial = cloneTemplate.querySelector('.select-item__material');
-  const optionMaterial = selectMaterial.querySelector('option');
-  
-  selectColor.innerHTML = '';
-  selectMaterial.innerHTML = '';
-  
-  getMock(selectColor, optionColor, COLORS);
-  getMock(selectMaterial, optionMaterial, MATERIALS);
-
-  fragment.appendChild(cloneTemplate);
-  body.appendChild(fragment);
-}
 
 renderTemplate();
 
